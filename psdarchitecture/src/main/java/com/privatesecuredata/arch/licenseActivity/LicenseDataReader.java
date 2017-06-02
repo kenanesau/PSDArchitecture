36fac5d493cb6eda8f070220ec084360f755ab5a
Kenan Esau <kenan.esau@conan.de>
2017-06-02 10:57:40 +0200

    Replaced lambdas with anonymous classes

--- psdarchitecture/src/main/java/com/privatesecuredata/arch/licenseActivity/LicenseDataReader.java
+++ psdarchitecture/src/main/java/com/privatesecuredata/arch/licenseActivity/LicenseDataReader.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import io.reactivex.Flowable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -26,17 +27,20 @@ public class LicenseDataReader {
     public static Flowable<StringBuilder> readLicense(Activity ctx, Product product) {
         return Flowable.just(product)
                 .subscribeOn(Schedulers.io())
-                .map(prod -> {
-                    InputStream licenseStream = ctx.getAssets().open(prod.getLicense().getFile());
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(licenseStream));
+                .map(new Function<Product, StringBuilder>() {
+                    @Override
+                    public StringBuilder apply(Product prod) throws Exception {
+                        InputStream licenseStream = ctx.getAssets().open(prod.getLicense().getFile());
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(licenseStream));
 
-                    StringBuilder sb = new StringBuilder();
-                    while (reader.ready()) {
-                        String line = reader.readLine();
-                        sb.append(line).append("\n");
+                        StringBuilder sb = new StringBuilder();
+                        while (reader.ready()) {
+                            String line = reader.readLine();
+                            sb.append(line).append("\n");
+                        }
+                        reader.close();
+                        return sb;
                     }
-                    reader.close();
-                    return sb;
                 });
     }
 
@@ -49,17 +53,20 @@ public class LicenseDataReader {
     public static Flowable<Product> readProduct(Activity ctx, String dir) {
         return Flowable.just(dir)
                 .subscribeOn(Schedulers.io())
-                .map(prodName -> {
-                    Product product = null;
-                    if (null != prodName) {
-                        try {
-                            product = Product.createProduct(ctx, prodName);
-                        } catch (IOException e) {
-                            throw new ArgumentException("Unable to restore state", e);
+                .map(new Function<String, Product>() {
+                    @Override
+                    public Product apply(String prodName) throws Exception {
+                        Product product = null;
+                        if (null != prodName) {
+                            try {
+                                product = Product.createProduct(ctx, prodName);
+                            } catch (IOException e) {
+                                throw new ArgumentException("Unable to restore state", e);
+                            }
                         }
-                    }
 
-                    return product;
+                        return product;
+                    }
                 });
     }
 }
--- psdarchitecture/src/main/java/com/privatesecuredata/arch/licenseActivity/ProductListFragment.java
+++ psdarchitecture/src/main/java/com/privatesecuredata/arch/licenseActivity/ProductListFragment.java
@@ -13,11 +13,16 @@ import android.view.ViewGroup;
 import com.privatesecuredata.arch.ui.widget.SimpleDivider;
 import com.privatesecuredata.arch.R;
 
+import org.reactivestreams.Publisher;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -49,8 +54,6 @@ public class ProductListFragment extends Fragment {
         RecyclerView rv = (RecyclerView)view.findViewById(R.id.lst_products);
 
         Bundle bundle = getArguments();
-        String productName = bundle.getString(ARG_PRODUCT_NAME, "ProductName");
-        String productVersion = bundle.getString(ARG_PRODUCT_VERSION, "V0.0.0.0");
 
         rv.setAdapter(adapter);
         rv.addItemDecoration(
@@ -58,45 +61,67 @@ public class ProductListFragment extends Fragment {
                         R.drawable.psdarch_simple_divider,
                         R.dimen.raster4));
 
-        Flowable.just("licenses")
+        Flowable.defer(new Callable<Publisher<? extends Boolean>>() {
+            @Override
+            public Publisher<? extends Boolean> call() throws Exception {
+                return Flowable.just(true);
+            }
+        })
             .subscribeOn(Schedulers.io())
-            .map( val -> getActivity().getAssets().list(val) )
-            .map( prodList -> {
-                List lst = new ArrayList<Product>(prodList.length);
-
-                for(String prod : prodList) {
-                    Product product = Product.createProduct(getActivity(), prod);
-                    lst.add(product);
+            .map(new Function<Boolean, String[]>() {
+                @Override
+                public String[] apply(Boolean val) throws Exception {
+                    return ProductListFragment.this.getActivity().getAssets().list("licenses");
                 }
+            })
+            .map(new Function<String[], List>() {
+                @Override
+                public List apply(String[] prodList) throws Exception {
+                    List lst = new ArrayList<Product>(prodList.length);
 
-                return lst;
+                    for (String prod : prodList) {
+                        Product product = Product.createProduct(ProductListFragment.this.getActivity(), prod);
+                        lst.add(product);
+                    }
+
+                    return lst;
+                }
             })
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                    productList -> {
-                        adapter.setItems(productList);
-                        adapter.setOnClickCb( (v, pos) -> {
-                            Product prod = adapter.get(pos);
+                    new Consumer<List>() {
+                        @Override
+                        public void accept(List productList) throws Exception {
+                            adapter.setItems(productList);
+                            adapter.setOnClickCb(new ProductListAdapter.IClickListener() {
+                                @Override
+                                public void onClick(View v, int pos) {
+                                    Product prod = adapter.get(pos);
 
-                            FragmentManager fragMan = getActivity().getSupportFragmentManager();
-                            Fragment frag = fragMan.findFragmentByTag(LicenseContentFragment.TAG);
+                                    FragmentManager fragMan = ProductListFragment.this.getActivity().getSupportFragmentManager();
+                                    Fragment frag = fragMan.findFragmentByTag(LicenseContentFragment.TAG);
 
-                            if (frag == null) {
-                                Fragment productListFrag = fragMan.findFragmentById(R.id.license_main_content);
-                                LicenseContentFragment licenceContentFrag = new LicenseContentFragment();
-                                licenceContentFrag.setProduct(prod);
-                                fragMan.beginTransaction()
-                                        .remove(productListFrag)
-                                        .add(R.id.license_main_content, licenceContentFrag, LicenseContentFragment.TAG)
-                                        .addToBackStack(null)
-                                        .commit();
-                            }
-                        } );
+                                    if (frag == null) {
+                                        Fragment productListFrag = fragMan.findFragmentById(R.id.license_main_content);
+                                        LicenseContentFragment licenceContentFrag = new LicenseContentFragment();
+                                        licenceContentFrag.setProduct(prod);
+                                        fragMan.beginTransaction()
+                                                .remove(productListFrag)
+                                                .add(R.id.license_main_content, licenceContentFrag, LicenseContentFragment.TAG)
+                                                .addToBackStack(null)
+                                                .commit();
+                                    }
+                                }
+                            });
+                        }
                     },
-                    ex -> {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    new Consumer<Throwable>() {
+                        @Override
+                        public void accept(Throwable ex) throws Exception {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(ProductListFragment.this.getActivity());
 
-                        builder.setMessage("Error reading the product list");
+                            builder.setMessage("Error reading the product list");
+                        }
                     });
 
         return view;
